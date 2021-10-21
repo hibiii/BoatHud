d741c13330ab65547ad40b920932b4131f87f251
Hibi <69405847+Hibiii@users.noreply.github.com>
2021-10-21 16:28:59 -0300

    Add loads of gadgets onto the HUD

--- src/main/java/hibi/boathud/HudRenderer.java
+++ src/main/java/hibi/boathud/HudRenderer.java
@@ -17,6 +17,8 @@ extends DrawableHelper {
 	private static final double[] MIN_V =   {  0d, 10d, 40d};
 	private static final double[] MAX_V =   { 40d, 70d, 70d};
 	private static final double[] SCALE_V = {4.5d,  3d,  6d};
+	private static final int[] BAR_OFF = { 0, 10, 20};
+	private static final int[] BAR_ON =  { 5, 15, 25};
 
 	public HudRenderer(MinecraftClient client) {
 		this.client = client;
@@ -26,32 +28,34 @@ extends DrawableHelper {
 		this.scaledWidth = this.client.getWindow().getScaledWidth();
 		this.scaledHeight = this.client.getWindow().getScaledHeight();
 		RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
-		// RenderSystem.setShader(GameRenderer::getPositionTexShader);
 		RenderSystem.setShaderTexture(0, WIDGETS_TEXTURE);
 		RenderSystem.enableBlend();
 		RenderSystem.defaultBlendFunc();
-		// int oldZOffset = this.getZOffset();
-		// this.setZOffset(oldZOffset +1);
 
 		int i = this.scaledWidth / 2;
 		// Overlay texture and bar //
 		this.drawTexture(stack, i - 91, this.scaledHeight - 82, 0, 67, 182, 30);
 		this.renderBar(stack, i - 91, this.scaledHeight - 82);
 
-		this.client.textRenderer.drawWithShadow(stack, Double.toString(Common.hudData.angleDiff), i - 90, this.scaledHeight - 76, 0xFFFFFF);
+		this.typeCentered(stack, String.format(Config.speedFormat, Common.hudData.speed * Config.speedRate), i - 58, this.scaledHeight - 76, 0xFFFFFF);
+		this.typeCentered(stack, String.format(Config.diffFormat, Common.hudData.angleDiff), i, this.scaledHeight - 76, 0xFFFFFF);
+		this.typeCentered(stack, String.format(Config.gFormat, Common.hudData.g), i + 58, this.scaledHeight - 76, 0xFFFFFF);
+		this.client.textRenderer.drawWithShadow(stack, Common.hudData.name, i + 89 - this.client.textRenderer.getWidth(Common.hudData.name), this.scaledHeight - 65, 0xFFFFFF);
 		RenderSystem.disableBlend();
-		// this.setZOffset(oldZOffset);
 	}
 
 	private void renderBar(MatrixStack stack, int x, int y) {
-		this.drawTexture(stack, x, y, 0, 0, 182, 5);
-		int barType = 0;
-		if(Common.hudData.speed < MIN_V[barType]) return;
-		if(Common.hudData.speed > MAX_V[barType]) {
+		this.drawTexture(stack, x, y, 0, BAR_OFF[Config.barType], 182, 5);
+		if(Common.hudData.speed < MIN_V[Config.barType]) return;
+		if(Common.hudData.speed > MAX_V[Config.barType]) {
 			if(this.client.world.getTime() % 2 == 0) return;
-			this.drawTexture(stack, x, y, 0, 5, 182, 5);
+			this.drawTexture(stack, x, y, 0, BAR_ON[Config.barType], 182, 5);
 			return;
 		}
-		this.drawTexture(stack, x, y, 0, 5, (int)((Common.hudData.speed - MIN_V[barType]) * SCALE_V[barType]), 5);
+		this.drawTexture(stack, x, y, 0, BAR_ON[Config.barType], (int)((Common.hudData.speed - MIN_V[Config.barType]) * SCALE_V[Config.barType]), 5);
+	}
+
+	private void typeCentered(MatrixStack stack, String text, int centerX, int y, int color) {
+		this.client.textRenderer.drawWithShadow(stack, text, centerX - this.client.textRenderer.getWidth(text) / 2, y, color);
 	}
 }
