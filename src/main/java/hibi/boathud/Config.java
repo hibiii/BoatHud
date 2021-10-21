8e9f1057aa011fa63504e05309e76c45ff212fab
Hibi <69405847+Hibiii@users.noreply.github.com>
2021-10-21 16:27:29 -0300

    Create Config.java

---
+++ src/main/java/hibi/boathud/Config.java
@@ -0,0 +1,12 @@
+package hibi.boathud;
+
+public class Config {
+	
+	public static double speedRate = 3.6d;
+	public static String speedFormat = "%03.0f km/h";
+	public static String diffFormat = "%03.0f °";
+	public static String gFormat = "%.1f g";
+	public static int barType = 0;
+
+	private Config() {}
+}
